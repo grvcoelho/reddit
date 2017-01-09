4ddcf0869445724c3cfdb0db85ae3158d8e01fb9
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-01-08 22:18:40 -0200

    Fix lint warnings

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import App from './App'
-import DevTools from './Devtools'
+import DevTools from './DevTools'
 
 const Root = ({ store }) => (
   <Provider store={store}>
